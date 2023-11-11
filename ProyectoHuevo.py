cc33f6ec4a882a2b49ca9c3672f0017962933fc9
debernall <84064985+debernall@users.noreply.github.com>
2023-11-10 19:53:01 -0500

    Optimización matriz de espacio
    Hice mejora sobre el cálculo de la matriz de espacio, reduje el consumo de memoria y el tiempo de procesamiento

--- ProyectoHuevo.py
+++ ProyectoHuevo.py
@@ -1,58 +1,54 @@
 import numpy as np
 import matplotlib.pylab as plt
+import time
 
 # Malla cúbica que contiene las condiciones iniciales del sistema, en el interior se encontrará el huevo esférico.
 
-# Inicialmente generaré una matriz de 3x3 con los valores 1 y 0 que me van a definir el espacio interno y externo al huevo.
+# Inicialmente generaré una matriz de 3x3 con los valores True y False que me van a definir el espacio interno y externo al huevo.
+
+# Constantes de la simulación. 
+# Construcción de la matriz cúbica del espacio, n es el tamaño de cada vector que compone la matriz del espacio.
+# En este caso elijo n=50, por tanto n^3=125.000 son los puntos que componen la matriz del espacio
+n = 500                                       #500 es un buen numero consumo 250MB
+
+# m es el valor máximo y mínimo alcanzado por la matriz del espacio. En este caso se define un espacio formado entre
+# -25 y 25 en x,y,z. Las unidades están dadas en micrómetros
+m = 250                                          #Determina el max y el min valor del espacio   25000micrometros lo reduzco a 250
+d = np.int(2*m/(n-1)) 
+
+# En este caso cada punto representa un espacio equivalente a (xxxxxxx PENDIENTE POR CALCULAR)                                   
+
+# Constantes para la formación del huevo, en este caso el huevo es esférico. r=24mm
+R = 240                                          
+
+# Creación de 3 matrices que representan las coordenadas x,y,z para cada punto dentro de la matriz de espacio
+t0 = time.time()
+
+m1 = np.arange(-m,m+1,d,dtype=np.int32)                        # m1 es un arreglo de tamaño n que sirve de base para la construcción de las matrices x,y,z
+x, y, z = np.meshgrid(m1,m1,m1, indexing='ij')                 # Creación de las matrices x,y,z. El orden de los vectores se toma de manera conveniente
+
+# Matriz del espacio      
+# Creo una matriz similar a las máscaras de numpy usando lo siguiente:
 
 # Tomo una sección transversal al huevo que me representa un corte transversal al plano x=0.
-# La curva que me delimita esta sección cumple con la relación y^2+z^2=r^2
-# Selecciono solamente la función y = sqrt(r**2-z**2)
-# Dado que el huevo tendrá simetría de revolución, "y" me representará el radio para cada punto a lo largo de z
-# Como me interesa usar un espacio mas grande 3D mas grande que el huevo, para los valores de z que no pertenecen a  
+# La curva que me delimita esta sección cumple con la relación r^2+z^2=R^, donde r puede ser y o x o cualquier vector en el plano z.
+# Selecciono solamente la función r = sqrt(R**2-z**2)
+# Dado que el huevo tendrá simetría de revolución, "r" me representará el radio para cada punto a lo largo de z
+# Como me interesa usar un espacio 3D mas grande que el huevo, para los valores de z fuera de la superficie del huevo: el radio=False 
 
-def radio(z,r):
-    if np.abs(z)<r:
-        radio = np.sqrt((r**2)-(z**2))
-    else:
-        radio = 0
-    return radio
+R_xy = (x*x)+(y*y)
+R_z = (R*R)-(z*z)
+M = R_xy<=R_z
 
-def espaciointerno(x,y,r):
-    p = np.sqrt(x**2+y**2)
-    if p<=r:
-        out=0
-    else:
-        out=100
-    return out
-    
-    
-n = 50                                          #Tamaño de los vectores n=250
-m = 25                                          #Determina el max y el min valor del espacio                                       
-r = 24                                          #Radio max del huevo
+tf = time.time()
 
-m1 = np.linspace(m,-m,n)
-m2 = np.linspace(-m,m,n)
-
-y, x, z = np.meshgrid(m1,m1,m1)
-      
-M=np.ones((n,n,n))
-for k in range(n):
-    radio_z = radio(z[0][0][k],r)
-    for j in range(n):
-        for i in range(n):
-            M[i][j][k] = espaciointerno(x[i][0][0],y[0][j][0],radio_z)
+print("Tiempo: "+str(tf-t0))
+print("Tamaño en MB: "+str(x.nbytes/1000000))
 
 fig = plt.figure()
 ax = fig.add_subplot(111, projection='3d')
-
 scatter = ax.scatter3D(x, y, z, c=M, cmap='gray', s=0.001)
-
 plt.show()
-# for i in range(n):
-#   for j in range(n):
-#     if (X[i][j]**2+Y[i][j]**2)<r**2:
-#       M[i][j] = 0
 
 
 # Implementación del método de diferencias finitas para la ecuación de difusión de calor, usando una función
