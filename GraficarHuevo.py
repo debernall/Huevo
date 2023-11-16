3c37007a129ca2c772e7797bde48f579095b413b
debernall <84064985+debernall@users.noreply.github.com>
2023-11-16 12:58:39 -0500

    Ajuste de la gráfica 3d

--- GraficarHuevo.py
+++ GraficarHuevo.py
@@ -2,12 +2,21 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 datos = np.genfromtxt("Matriz.dat")
-datos = datos.reshape((100,100,100))
+datos = datos.reshape((50,50,50))
+
+n = 50                                       			#500 es un buen número, consumo 250MB
+m = 25.0								# m es el valor máximo y mínimo alcanzado por la matriz del espacio. En este caso 	
+								# se define un espacio formado entre -25 y 25 en x,y,z. Unidades m x 10^-4 
+d = (2*m/(n)) 					# Delta, distancia entre cada punto. d=5. Uso np.int32 para reducir procesamiento
+
+m1 = np.arange(-m,m,d)                         # m1 es un arreglo de tamaño n que sirve de base para la construcción de las matrices x,y,z
+x, y, z = np.meshgrid(m1,m1,m1, indexing='ij')  
 
 fig = plt.figure()
 ax = fig.add_subplot(111, projection='3d')
-scatter = ax.voxels(datos, edgecolor='k')                 #Gráfica de la cáscara
-ax.view_init(0, 90)
+scatter = ax.scatter3D(x[:25,:,:], y[:25,:,:], z[:25,:,:], c=datos[:25,:,:], cmap='viridis', s=0.1)                 #Gráfica de la cáscara
+ax.view_init(45, 45)
 plt.show()
+
 
 
--- ProyectoHuevo.cpp
+++ ProyectoHuevo.cpp
@@ -9,26 +9,33 @@ int main () {
 	ofstream outfile;
 	outfile.open("Matriz.dat");
 
-	const double pi = M_PI;
-	int n = 100;
-	double m = 25.0;
-	double d = (2*m)/(n-1);
+	const double pi = M_PI;					//Pi
+	int n = 50;						//Tamaño n^3
+	double m = 25.0;					//Distancia máxima en cada eje desde el origen
+	double d = (2*m)/(n-1);					//Delta, ancho de cada cuadrícula
 
-	double Rc = 24.0;
-	double Ra = 23.7;
-	double Ry = 10.0;
+	double Rc = 24.0;					//Radio máximo de la cáscara
+	double Ra = 23.7;					//Radio máximo de la albúmina
+	double Ry = 10.0;					//Radio máximo de la yema
 
-	double g = tan(8*pi/180);
+	double g = tan(8*pi/180);				//Tangente en el punto medio del perfíl de huevo 
 
-	double A[n][n][n];
+	double A[n][n][n];	
 
 	double x;
 	double y;
 	double z;
 	double r2;
-	double R2;
-	double a = 24.5;
-	double b = 16.5;
+	double R2_c;
+	double R2_a;
+	double R2_y;
+	double a_a = 24.2;					//Longitud del semieje mayor albúmina
+	double b_a = 16.2;					//Longitud del semieje menor albúmina
+	double a_c = 24.5;					//Longitud del semieje mayor cáscara
+	double b_c = 16.5;					//Longitud del semieje menor cáscara
+	double alpha_y = 1;					//Difusividad términa de la yema
+	double alpha_a = 2;					//Difusividad térmica de la albúmina
+	double alpha_c = 3;					//Difusividad térmica de la cáscara
 
 	for (int i=0;i<n;i++){
 		for (int j=0; j<n;j++){
@@ -38,13 +45,22 @@ int main () {
 				z = (k*d)-m;
 						
 				r2 = x*x+y*y;
-				R2 = (1-((z*z)/(a*a)))*(b+(z*g))*(b+(z*g));
-
-				if (r2>R2){
+				R2_c = (1-((z*z)/(a_c*a_c)))*(b_c+(z*g))*(b_c+(z*g));
+				R2_a = (1-((z*z)/(a_a*a_a)))*(b_a+(z*g))*(b_a+(z*g));
+				R2_y = Ry*Ry - z*z;
+				if (r2>R2_c){
 					A[i][j][k]=0;
 				}
 				else {
-					A[i][j][k]=1;	
+					if (r2<=R2_y){
+						A[i][j][k]=0;
+					}
+					else if (r2<R2_a){
+						A[i][j][k]=200;	
+					}
+					else {
+						A[i][j][k]=300;
+					}	
 				}
 				outfile << A[i][j][k] << "\n";
 			}	
