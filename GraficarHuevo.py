382642754ab88ab3a8dfd0a586a1d444ec11ef79
debernall <84064985+debernall@users.noreply.github.com>
2023-11-21 02:41:35 -0500

    Implementación Dif Finitas
    Se implementa diferencias finitas (solo conducción), se mejora la representación gráfica, entre otros cambios.

--- GraficarHuevo.py
+++ GraficarHuevo.py
@@ -1,22 +1,24 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
 
-datos = np.genfromtxt("Matriz.dat")
-datos = datos.reshape((50,50,50))
 
-n = 50                                       			#500 es un buen número, consumo 250MB
-m = 25.0								# m es el valor máximo y mínimo alcanzado por la matriz del espacio. En este caso 	
-								# se define un espacio formado entre -25 y 25 en x,y,z. Unidades m x 10^-4 
-d = (2*m/(n)) 					# Delta, distancia entre cada punto. d=5. Uso np.int32 para reducir procesamiento
+Temp = np.genfromtxt("Matriz.dat")						#Importo la matriz de temperaturas almacenado como vector
+n = np.cbrt(np.shape(Temp)[0]).astype(int)					#Cálculo de la magnitud de cada vector de la matriz temperatura
+Temp = Temp.reshape((n,n,n))							#Reorganizo el vector en una matriz de nxnxn
 
-m1 = np.arange(-m,m,d)                         # m1 es un arreglo de tamaño n que sirve de base para la construcción de las matrices x,y,z
-x, y, z = np.meshgrid(m1,m1,m1, indexing='ij')  
+m = 25.0									#Distancia máxima en cada eje desde el origen	
+d = (2*m/(n)) 									#Delta, separación entre cada nodo
 
-fig = plt.figure()
-ax = fig.add_subplot(111, projection='3d')
-scatter = ax.scatter3D(x[:25,:,:], y[:25,:,:], z[:25,:,:], c=datos[:25,:,:], cmap='viridis', s=0.1)                 #Gráfica de la cáscara
-ax.view_init(45, 45)
-plt.show()
+m1 = np.arange(-m,m,d)                         					#Arreglo de tamaño n que sirve de base para la construcción de las matrices x,y,z
+x, y, z = np.meshgrid(m1,m1,m1, indexing='ij')  		#Construcción de una red de puntos que me va a permitir graficar en un espacio una variable en un espacio 3 Dimensional
+
+fig = plt.figure()								#Inicialización de una figura con matplotlib
+ax = fig.add_subplot(111, projection='3d')					#Primer subplot
+scatter = ax.scatter3D(x.flatten(), y.flatten(), z.flatten(), c=datos.flatten(), cmap='hot', s=0.1/n)		#Gráfica de Temperatura, s es el tamaño de los nodos
+ax.view_init(45, 45)								#Ángulos iniciales de observación de la gráfica generada
+fig.colorbar(scatter, label='Temperatura')					#Barra lateral que indica la escala de temperatura en función del color presentado en la gráfica
+plt.show()									#CAMBIAR A IMPORTAR A JPG 
 
 
 
--- ProyectoHuevo.cpp
+++ ProyectoHuevo.cpp
@@ -2,168 +2,122 @@
 #include <cmath>
 #include <stdio.h>
 #include <fstream>
+#include <vector>
+#include <algorithm>
 
 using namespace std;
 
 int main () {
-	ofstream outfile;
-	outfile.open("Matriz.dat");
+	ofstream outfile;									//Inicialización variable de documento
+	outfile.open("Matriz.dat");								//Apertura de documento para escritura
 
-	const double pi = M_PI;					//Pi
-	int n = 110;						//Tamaño n^3
-	double m = 25.0;					//Distancia máxima en cada eje desde el origen
-	double d = (2*m)/(n-1);					//Delta, ancho de cada cuadrícula
+	const double pi = M_PI;									//Pi
+	int n = 100;										//Número de nodos en cada dirección de la matriz nxnxn
+	double m = 25.0;									//Distancia máxima en cada eje desde el origen
+	double dx = (2*m)/(n-1);								//Delta, separación entre cada nodo
 
-	double Ry = 10.0;					//Radio máximo de la yema
+	double Ry = 10.0;									//Radio máximo de la yema
+	double g = tan(8*pi/180);								//Tangente en el punto medio del perfíl de huevo 
+	double a_albu = 24.2;									//Longitud del semieje mayor albúmina
+	double b_albu = 19.2;									//Longitud del semieje menor albúmina
+	double a_casc = 24.6;									//Longitud del semieje mayor cáscara
+	double b_casc = 19.6;									//Longitud del semieje menor cáscara
 
-	double g = tan(8*pi/180);				//Tangente en el punto medio del perfíl de huevo 
+	vector<vector<vector<double>>> T(n, vector<vector<double>>(n, vector<double>(n)));	//Temperatura en t presente en cada nodo
+	vector<vector<vector<double>>> Tf(n, vector<vector<double>>(n, vector<double>(n)));	//Temperatura en t futuro en cada nodo
+	vector<vector<vector<double>>> Fo(n, vector<vector<double>>(n, vector<double>(n)));	//Número de Fourier para cada nodo
 
-	int A[n][n][n];	
-
-	double x;
-	double y;
-	double z;
-	double r2;
-	double R2_c;
-	double R2_a;
-	double R2_y;
-	double a_a = 24.2;					//Longitud del semieje mayor albúmina
-	double b_a = 16.2;					//Longitud del semieje menor albúmina
-	double a_c = 24.6;					//Longitud del semieje mayor cáscara
-	double b_c = 16.6;					//Longitud del semieje menor cáscara
-	double alpha_y = 1;					//Difusividad términa de la yema
-	double alpha_a = 2;					//Difusividad térmica de la albúmina
-	double alpha_c = 3;					//Difusividad térmica de la cáscara
-	int error=0;
-	for (int i=0;i<n;i++){
-		for (int j=0; j<n;j++){
-			for (int k=0; k<n; k++){
-				x = (i*d)-m;
-				y = (j*d)-m;
-				z = (k*d)-m;
-						
-				r2 = x*x+y*y;
-				R2_c = (1-((z*z)/(a_c*a_c)))*(b_c+(z*g))*(b_c+(z*g));
-				R2_a = (1-((z*z)/(a_a*a_a)))*(b_a+(z*g))*(b_a+(z*g));
-				R2_y = Ry*Ry - z*z;
-				if (r2>R2_c){
-					A[i][j][k]=0;
-				}
-				else {
-					if (r2<=R2_y){
-						A[i][j][k]=0;
-					}
-					else if (r2<=R2_a){
-						A[i][j][k]=0;	
-					}
-					else if (r2<=R2_c){
-						A[i][j][k]=1;
-					}	
-					else {
-						error = error +1;				
-					}
-				}
-				outfile << A[i][j][k] << "\n";
-			}	
-		}
-	}
+	double x,y,z;										//Inicialización de variables de longitud
+	double r2,R2_casc,R2_albu,R2_yema;							//Inicialización de variables de radio
 	
-	cout << " error" << error << "\n";
+	double a = 0.000002;									//Difusividad térmica (PENDIENTE POR CAMBIAR - VALOR DE PRUEBA)
+	double dt = 0.8*dx*dx/(6*a);								//Diferencial de tiempo calculado en base a la condición de estabilidad
+	double t_muestreo = 100000;								//Tiempo de muestreo máximo (PENDIENTE)
+	int tf = int(t_muestreo/dt);								//Número máximo de intervalos de tiempo
 	
-	
-	int ss[n][n][n];
-	int s1=0;
-	int s2=0;
-	int s3=0;
-	int s4=0;
-	int s5=0;
-	int s6=0;
-	int s0=0;
-	int se=0;
-	int s1n=0;
-	int s2n=0;
-	int s3n=0;
-	int s4n=0;
-	int s5n=0;
-	int s6n=0;
-	int s0n=0;
-	int sen=0;
-	int cont = 0;
-	int ttt = 0;
+	//CONDICIONES INICIALES
+	double T0_Huevo=40;									//Temperatura inicial Huevo
+	double T0_Aire=25;									//Temperatura inicial del Aire circundante
 	
 	for (int i=0;i<n;i++){
 		for (int j=0; j<n;j++){
 			for (int k=0; k<n; k++){
-				x = (i*d)-m;
-				y = (j*d)-m;
-				z = (k*d)-m;
+				x = (i*dx)-m;							//Posiciones en mm medidas desde el centro de la red de nodos
+				y = (j*dx)-m;
+				z = (k*dx)-m;
 						
-				r2 = x*x+y*y;
-				R2_c = (1-((z*z)/(a_c*a_c)))*(b_c+(z*g))*(b_c+(z*g));
-				R2_a = (1-((z*z)/(a_a*a_a)))*(b_a+(z*g))*(b_a+(z*g));
-				R2_y = Ry*Ry - z*z;
-				if (r2>R2_c){
-					ttt =0;
+				r2 = x*x+z*z;								//Distancia de cada punto sobre sucesivos planos y al eje y
+				R2_casc = (1-((y*y)/(a_casc*a_casc)))*(b_casc+(y*g))*(b_casc+(y*g));	//Distancia máxima de la cascara sobre sucesivos planos y al eje y
+				R2_albu = (1-((y*y)/(a_albu*a_albu)))*(b_albu+(y*g))*(b_albu+(y*g));	//Distancia máxima de la albúmina sobre sucesivos planos y al eje y
+				R2_yema = Ry*Ry - y*y;							//Distancia máxima de la yema sobre sucesivos planos y al eje y
+				
+				if (r2>R2_casc){						//Nodos externos al huevo
+					T[i][j][k]=T0_Aire;						//Condición inicial de temperatura
+					Fo[i][j][k]=a*dt/(dx*dx);					//Número de Fourier del nodo
 				}
 				else {
-					if (r2<=R2_y){
-						ttt = 0;
+					if (r2<=R2_yema){					//Nodos pertenecientes a la yema
+						T[i][j][k]=100;						//Condición inicial de temperatura (TEMPERATURA DE PRUEBA)
+						Fo[i][j][k]=a*dt/(dx*dx);				//Número de Fourier del nodo
 					}
-					else if (r2<R2_a){
-						ttt =0;	
-						
+					else if (r2<=R2_albu){					//Nodos pertenecientes a la albúmina
+						T[i][j][k]=T0_Huevo;					//Condición inicial de temperatura
+						Fo[i][j][k]=a*dt/(dx*dx);				//Número de Fourier del nodo
 					}
-					else {
-						ttt =0;
-						cont = cont +1;
-						ss[i][j][k] = A[i+1][j][k]+A[i-1][j][k]+A[i][j+1][k]+A[i][j-1][k]+A[i][j][k+1]+A[i][j][k-1];
-						if (ss[i][j][k]==0){s0=s0+1;}
-						else if (ss[i][j][k]==1){s1=s1+1;}
-						else if (ss[i][j][k]==2){s2=s2+1;}
-						else if (ss[i][j][k]==3){s3=s3+1;}
-						else if (ss[i][j][k]==4){s4=s4+1;}
-						else if (ss[i][j][k]==5){s5=s5+1;}
-						else if (ss[i][j][k]==6){s6=s6+1;}
-						else if (ss[i][j][k]==-1){s1n=s1n+1;}
-						else if (ss[i][j][k]==-2){s2n=s2n+1;}
-						else if (ss[i][j][k]==-3){s3n=s3n+1;}
-						else if (ss[i][j][k]==-4){s4n=s4n+1;}
-						else if (ss[i][j][k]==-5){s5n=s5n+1;}
-						else if (ss[i][j][k]==-6){s6n=s6n+1;}
-						else {se=se+1;}
+					else {							//Nodos pertenecientes a la cáscara
+						T[i][j][k]=T0_Huevo;					//Condición inicial de temperatura
+						Fo[i][j][k]=a*dt/(dx*dx);				//Número de Fourier del nodo
 					}	
 				}
 			}	
 		}
-	}
+	}	
 	
-	/*
-	for (int i=1;i<n-1;i++){
-		for (int j=1; j<n-1;j++){
-			for (int k=1; k<n-1; k++){
-				cont = cont +1;
-				ss[i][j][k] = (A[i+1][j][k]+A[i-1][j][k]+A[i][j+1][k]+A[i][j-1][k]+A[i][j][k+1]+A[i][j][k-1])*A[i][j][k];
-				if (ss[i][j][k]==0){s0=s0+1;}
-				else if (ss[i][j][k]==1){s1=s1+1;}
-				else if (ss[i][j][k]==2){s2=s2+1;}
-				else if (ss[i][j][k]==3){s3=s3+1;}
-				else if (ss[i][j][k]==4){s4=s4+1;}
-				else if (ss[i][j][k]==5){s5=s5+1;}
-				else if (ss[i][j][k]==6){s6=s6+1;}
-				else if (ss[i][j][k]==-1){s1n=s1n+1;}
-				else if (ss[i][j][k]==-2){s2n=s2n+1;}
-				else if (ss[i][j][k]==-3){s3n=s3n+1;}
-				else if (ss[i][j][k]==-4){s4n=s4n+1;}
-				else if (ss[i][j][k]==-5){s5n=s5n+1;}
-				else if (ss[i][j][k]==-6){s6n=s6n+1;}
-				else {se=se+1;}
+	
+	//EVOLUCIÓN TEMPORAL
+	double Fo_i;								//Inicialización de variable Número de Fourier para reducir la extensión de la ecuación de calor
+	
+	for (int t=0; t<tf; t++){
+		for (int i=0;i<n;i++){
+			for (int j=0; j<n;j++){
+				for (int k=0; k<n; k++){
+					Fo_i = Fo[i][j][k];					//Almaceno el valor de Fo del nodo en una variable double para simplificar escritura
+					x = (i*dx)-m;						//Posiciones en mm medidas desde el centro de la red de nodos
+					y = (j*dx)-m;
+					z = (k*dx)-m;
+						
+					r2 = x*x+z*z;								//Distancia de cada punto sobre sucesivos planos y al eje y
+					R2_casc = (1-((y*y)/(a_casc*a_casc)))*(b_casc+(y*g))*(b_casc+(y*g));	//Distancia máxima de la cascara sobre sucesivos planos y al eje y
+					
+					if (r2>R2_casc){							
+						Tf[i][j][k]=25;					//CONDICIÓN DE CONTORNO: El aire circundante se mantiene a temperatura constante
+					
+					}
+					else {							//Temperatura futura por medio del método diferencias finitas - balance de energía
+						Tf[i][j][k]=Fo_i*((T[i][j][k]*((1/Fo_i)-6))+T[i+1][j][k]+T[i-1][j][k]+T[i][j+1][k]+T[i][j-1][k]+T[i][j][k+1]+T[i][j][k-1]);	
+					}							//FALTA INCLUIR EL TÉRMINO DE GENERACIÓN, CONVECCIÓN Y MATRIZ DE GENERACIÓN.
+				}
+			}
+		}
+		for (int i=0;i<n;i++){								//Almaceno la Temperatura futura calculada en Temperatura presente para el sig ciclo 
+			for (int j=0; j<n;j++){
+				for (int k=0; k<n; k++){
+					T[i][j][k]=Tf[i][j][k];
+				}
 			}
 		}
 	}
-	*/
-	cout << " s0=" << s0 << "\n s1=" << s1 << "\n s2=" << s2 << "\n s3=" << s3 << "\n s4=" << s4 << "\n s5=" << s5 << "\n s6=" << s6 << "\n se=" << se; 
-	cout << "\n s1n=" << s1n << "\n s2n=" << s2n << "\n s3n=" << s3n << "\n s4n=" << s4n << "\n s5n=" << s5n << "\n s6n=" << s6n ; 
-	cout << "\n cont=" << cont;
-	outfile.close();
+	
+	//IMPRESIÓN A ARCHIVO EXTERNO
+	for (int i=0;i<n;i++){
+			for (int j=0; j<n;j++){
+				for (int k=0; k<n; k++){
+					outfile << T[i][j][k] << "\n";				//Almaceno la última temperatura en un documento externo
+				}
+			}
+		}
+
+
+	outfile.close();									//Cierre del documento para escritura
 	return 0;
 }
